f0c880d55cacd36f190c03234e77506f35d20a50
vipgirisim <info@vipgirisim.com.tr>
2021-02-16 01:21:46 +0300

    son deme
    son deme

--- app/routes/turorial.routes.js
+++ app/routes/turorial.routes.js
@@ -1,41 +1,39 @@
-module.exports = app => { 
-  
-   const cors = require("cors");
-   var router = require("express").Router();
-
-  var corsOptions = {
-    origin: 'https://vipfal.herokuapp.com/admin/',
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-  }
-
 module.exports = app => {
- 
-  router.use(cors(corsOptions)) 
   const tutorials = require("../controllers/tutorial.controller.js");
 
   var router = require("express").Router();
+  router.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
 
-  // Create a new Tutorial
-  router.post("/", tutorials.create);
+    var corsOptions = {
+      origin: 'https://vipfal.herokuapp.com/',
+      optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+    }
 
-  // Retrieve all Tutorials
-  router.get("/", tutorials.findAll);
+    router.use(cors(corsOptions))
 
-  // Retrieve all published Tutorials
-  router.get("/published", tutorials.findAllPublished);
+    // Create a new Tutorial
+    router.post("/", tutorials.create);
 
-  // Retrieve a single Tutorial with id
-  router.get("/:id", tutorials.findOne);
+    // Retrieve all Tutorials
+    router.get("/", tutorials.findAll);
 
-  // Update a Tutorial with id
-  router.put("/:id", tutorials.update);
+    // Retrieve all published Tutorials
+    router.get("/published", tutorials.findAllPublished);
 
-  // Delete a Tutorial with id
-  router.delete("/:id", tutorials.delete);
+    // Retrieve a single Tutorial with id
+    router.get("/:id", tutorials.findOne);
 
-  // Create a new Tutorial
-  router.delete("/", tutorials.deleteAll);
+    // Update a Tutorial with id
+    router.put("/:id", tutorials.update);
 
-  app.use("/admin/tutorials", router);
+    // Delete a Tutorial with id
+    router.delete("/:id", tutorials.delete);
+
+    // Create a new Tutorial
+    router.delete("/", tutorials.deleteAll);
+
+    app.use("/admin/tutorials", router);
+    next();
+  });
 };
-}
