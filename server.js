fe400dc1d0e957b4f345b3912dba8fccd6e88794
vipgirisim <info@vipgirisim.com.tr>
2021-02-16 02:09:07 +0300

    t
    t

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ require("./app/routes/turorial.routes")(app);
 
 
 // set port, listen for requests
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 const server=app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
