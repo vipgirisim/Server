52281aa961426cc4947f19a01916cbdf55c39064
vipgirisim <info@vipgirisim.com.tr>
2021-02-15 13:31:49 +0300

    j
    j

--- app/routes/turorial.routes.js
+++ app/routes/turorial.routes.js
@@ -1,17 +1,14 @@
-const cors = require("cors");
-var router = require("express").Router();
-
-
-var corsOptions = {
-  origin: 'https://vipfal.herokuapp.com/',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
-
 module.exports = app => {
+  var corsOptions = {
+    origin: 'https://vipfal.herokuapp.com/admin/',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }
+  const cors = require("cors");
+  var router = require("express").Router();
   router.use(cors(corsOptions))
   const tutorials = require("../controllers/tutorial.controller.js");
 
- 
+
   // Create a new Tutorial
   router.post("/", tutorials.create);
 
@@ -33,5 +30,5 @@ module.exports = app => {
   // Create a new Tutorial
   router.delete("/", tutorials.deleteAll);
 
-  app.use("/api/tutorials", router);
+  app.use("/admin/tutorials", router);
 };
