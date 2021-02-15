924960e1043a742b0eef2f740727ca15dbfe8014
vipgirisim <78596824+vipgirisim@users.noreply.github.com>
2021-02-16 02:10:05 +0300

    Update turorial.routes.js

--- app/routes/turorial.routes.js
+++ app/routes/turorial.routes.js
@@ -2,38 +2,27 @@ module.exports = app => {
   const tutorials = require("../controllers/tutorial.controller.js");
 
   var router = require("express").Router();
-  router.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
 
-    var corsOptions = {
-      origin: 'https://vipfal.herokuapp.com/',
-      optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-    }
+  // Create a new Tutorial
+  router.post("/", tutorials.create);
 
-    router.use(cors(corsOptions))
+  // Retrieve all Tutorials
+  router.get("/", tutorials.findAll);
 
-    // Create a new Tutorial
-    router.post("/", tutorials.create);
+  // Retrieve all published Tutorials
+  router.get("/published", tutorials.findAllPublished);
 
-    // Retrieve all Tutorials
-    router.get("/", tutorials.findAll);
+  // Retrieve a single Tutorial with id
+  router.get("/:id", tutorials.findOne);
 
-    // Retrieve all published Tutorials
-    router.get("/published", tutorials.findAllPublished);
+  // Update a Tutorial with id
+  router.put("/:id", tutorials.update);
 
-    // Retrieve a single Tutorial with id
-    router.get("/:id", tutorials.findOne);
+  // Delete a Tutorial with id
+  router.delete("/:id", tutorials.delete);
 
-    // Update a Tutorial with id
-    router.put("/:id", tutorials.update);
+  // Create a new Tutorial
+  router.delete("/", tutorials.deleteAll);
 
-    // Delete a Tutorial with id
-    router.delete("/:id", tutorials.delete);
-
-    // Create a new Tutorial
-    router.delete("/", tutorials.deleteAll);
-
-    router.use("/admin/tutorials", router);
-    next();
-  });
+  app.use("/admin/tutorials", router);
 };
