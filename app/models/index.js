4b77f0ef774860a9767c2db69aa6b4bdd98b994a
vipgirisim <info@vipgirisim.com.tr>
2021-02-16 06:17:09 +0300

    son deneme
    son deneme

--- app/models/index.js
+++ app/models/index.js
@@ -7,6 +7,8 @@ db.mongoose = mongoose;
 
 db.user = require("./user.model");
 db.role = require("./role.model");
+db.role = require("./tutorial.model")(mongoose);
+
 
 db.ROLES = ["user", "admin", "moderator","falci"];
 
