a25eeb4724c4b4324df9fc2a7c3755f94f40ac09
vipgirisim <info@vipgirisim.com.tr>
2021-02-16 04:13:16 +0300

    u

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ require("./app/routes/auth.routes")(app);
 require("./app/routes/user.routes")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 const server=app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
