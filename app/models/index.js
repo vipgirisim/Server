0b010b6c71e8c0eed4d90ddd24aee85e94cb3b1b
vipgirisim <info@vipgirisim.com.tr>
2021-02-16 04:16:11 +0300

    oo
    oo

--- app/models/index.js
+++ app/models/index.js
@@ -7,6 +7,7 @@ db.mongoose = mongoose;
 
 db.user = require("./user.model");
 db.role = require("./role.model");
+db.role = require("./tutorial.model")(mongoose);
 
 db.ROLES = ["user", "admin", "moderator","falci"];
 
