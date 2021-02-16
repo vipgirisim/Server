bf2dc1be187047333149461d4b68bbb4ed9bad95
vipgirisim <info@vipgirisim.com.tr>
2021-02-16 03:06:26 +0300

    oo
    oo

--- src/app/routes/user.routes.js
+++ src/app/routes/user.routes.js
@@ -2,9 +2,11 @@ const { authJwt } = require("../middlewares");
 const controller = require("../controllers/user.controller");
 
 module.exports = function(app) {
+  
   app.use(function(req, res, next) {
-     res.header('Access-Control-Allow-Origin', '*');
-  next();
+    res.header('Access-Control-Allow-Origin', '*');
+
+    next();
   });
 
   app.get("/api/test/all", controller.allAccess);
