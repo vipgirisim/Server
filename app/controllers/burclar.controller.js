7e2c03be97f7fe69836032fd18190f63ed0559be
vipgirisim <info@vipgirisim.com.tr>
2021-02-22 00:38:59 +0300

    dd
    dd

--- app/controllers/burclar.controller.js
+++ app/controllers/burclar.controller.js
@@ -1,7 +1,7 @@
 const db = require("../models");
 const Burclar = db.burclar;
 
-// Create and Save a new Tutorial
+// Create and Save a new burclar
 exports.create = (req, res) => {
   // Validate request
   if (!req.body.burcadi) {
@@ -9,14 +9,14 @@ exports.create = (req, res) => {
     return;
   }
 
-  // Create a Tutorial
+  // Create a burclar
   const burclar = new Burclar({
     burcadi: req.body.burcadi,
     burclinki: req.body.burclinki,
    // published: req.body.published ? req.body.published : false
   });
 
-  // Save Tutorial in the database
+  // Save burclar in the database
   burclar
     .save(burcadi)
     .then(data => {
@@ -25,12 +25,12 @@ exports.create = (req, res) => {
     .catch(err => {
       res.status(500).send({
         message:
-          err.message || "Some error occurred while creating the Tutorial."
+          err.message || "Some error occurred while creating the burclar"
       });
     });
 };
 
-// Retrieve all Tutorials from the database.
+// Retrieve all burclar from the database.
 exports.findAll = (req, res) => {
   const burcadi = req.query.burcadi;
   var condition = burcadi ? { burcadi: { $regex: new RegExp(burcadi), $options: "i" } } : {};
@@ -42,29 +42,29 @@ exports.findAll = (req, res) => {
     .catch(err => {
       res.status(500).send({
         message:
-          err.message || "Some error occurred while retrieving tutorials."
+          err.message || "Some error occurred while retrieving burclar."
       });
     });
 };
 
-// Find a single Tutorial with an id
+// Find a single burclar with an id
 exports.findOne = (req, res) => {
   const id = req.params.id;
 
   Burclar.findById(id)
     .then(data => {
       if (!data)
-        res.status(404).send({ message: "Not found Tutorial with id " + id });
+        res.status(404).send({ message: "Not found burclar with id " + id });
       else res.send(data);
     })
     .catch(err => {
       res
         .status(500)
-        .send({ message: "Error retrieving Tutorial with id=" + id });
+        .send({ message: "Error retrieving burclar with id=" + id });
     });
 };
 
-// Update a Tutorial by the id in the request
+// Update a burclar by the id in the request
 exports.update = (req, res) => {
   if (!req.body) {
     return res.status(400).send({
@@ -78,18 +78,18 @@ exports.update = (req, res) => {
     .then(data => {
       if (!data) {
         res.status(404).send({
-          message: `Cannot update Tutorial with id=${id}. Maybe Tutorial was not found!`
+          message: `Cannot update burclar with id=${id}. Maybe burclar was not found!`
         });
-      } else res.send({ message: "Tutorial was updated successfully." });
+      } else res.send({ message: "burclar was updated successfully." });
     })
     .catch(err => {
       res.status(500).send({
-        message: "Error updating Tutorial with id=" + id
+        message: "Error updating burclar with id=" + id
       });
     });
 };
 
-// Delete a Tutorial with the specified id in the request
+// Delete a burclar with the specified id in the request
 exports.delete = (req, res) => {
   const id = req.params.id;
 
@@ -97,38 +97,38 @@ exports.delete = (req, res) => {
     .then(data => {
       if (!data) {
         res.status(404).send({
-          message: `Cannot delete Tutorial with id=${id}. Maybe Tutorial was not found!`
+          message: `Cannot delete burclar with id=${id}. Maybe burclar was not found!`
         });
       } else {
         res.send({
-          message: "Tutorial was deleted successfully!"
+          message: "burclar was deleted successfully!"
         });
       }
     })
     .catch(err => {
       res.status(500).send({
-        message: "Could not delete Tutorial with id=" + id
+        message: "Could not delete burclar with id=" + id
       });
     });
 };
 
-// Delete all Tutorials from the database.
+// Delete all burclar from the database.
 exports.deleteAll = (req, res) => {
   Burclar.deleteMany({})
     .then(data => {
       res.send({
-        message: `${data.deletedCount} Tutorials were deleted successfully!`
+        message: `${data.deletedCount} burclar were deleted successfully!`
       });
     })
     .catch(err => {
       res.status(500).send({
         message:
-          err.message || "Some error occurred while removing all tutorials."
+          err.message || "Some error occurred while removing all burclar."
       });
     });
 };
 
-// Find all published Tutorials
+// Find all published burclar
 exports.findAllPublished = (req, res) => {
   Burclar.find({ published: true })
     .then(data => {
@@ -137,7 +137,7 @@ exports.findAllPublished = (req, res) => {
     .catch(err => {
       res.status(500).send({
         message:
-          err.message || "Some error occurred while retrieving tutorials."
+          err.message || "Some error occurred while retrieving burclar."
       });
     });
 };
