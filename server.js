039f1caed4a5803d688e0f8de8dd343907a7cb5d
vipgirisim <info@vipgirisim.com.tr>
2021-03-02 01:01:50 +0300

    dd
    dd

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Headers","*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD"); 
-  
   next();
 }); 
 mongoose
